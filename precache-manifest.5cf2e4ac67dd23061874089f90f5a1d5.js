e55c1b00ea79796a7ca4606606d50480d1fe5ec9
zhangyue <zhangyue@fulan.com.cn>
2020-07-18 11:20:20 +0800

    init

---
+++ precache-manifest.5cf2e4ac67dd23061874089f90f5a1d5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c81b9e70df40e0a64e537f7119d515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "502b909eba67d720b753",
+    "url": "/static/css/2.c8ac8bf5.chunk.css"
+  },
+  {
+    "revision": "3a20e22b519db57c6418",
+    "url": "/static/css/main.2360740e.chunk.css"
+  },
+  {
+    "revision": "502b909eba67d720b753",
+    "url": "/static/js/2.d26a970d.chunk.js"
+  },
+  {
+    "revision": "3d669e94f91837166de5dcb402c232d1",
+    "url": "/static/js/2.d26a970d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a20e22b519db57c6418",
+    "url": "/static/js/main.23d89a25.chunk.js"
+  },
+  {
+    "revision": "c772838bf04e6e8c2ca1",
+    "url": "/static/js/runtime-main.ec2b0ac6.js"
+  },
+  {
+    "revision": "67318d248050ed72d6ef83cc6c659142",
+    "url": "/static/media/alipay.67318d24.png"
+  },
+  {
+    "revision": "98378c05fd5e7dbb0a786ebcc0343f43",
+    "url": "/static/media/img_home_bg.98378c05.png"
+  },
+  {
+    "revision": "0a839f5e3b5f8f08ab2e0536b052783e",
+    "url": "/static/media/img_map.0a839f5e.png"
+  },
+  {
+    "revision": "9d2144004c6b047c1eac069ccc670a8d",
+    "url": "/static/media/img_title_bg.9d214400.png"
+  }
+]);
